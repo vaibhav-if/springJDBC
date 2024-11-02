135a38d393da3de56d6682daa11301a0c22b1b2b
Vaibhav Agrawal <vaibhavagr711@gmail.com>
2024-11-02 10:41:58 +0530

    Add student class

--- src/main/java/com/example/springJDBC/SpringJdbcApplication.java
+++ src/main/java/com/example/springJDBC/SpringJdbcApplication.java
@@ -1,13 +1,16 @@
 package com.example.springJDBC;
 
+import com.example.springJDBC.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringJdbcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringJdbcApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringJdbcApplication.class, args);
+		Student s = context.getBean(Student.class);
 	}
 
 }
